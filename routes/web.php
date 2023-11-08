10e4a58dcbd16f25a34ff9e6fa2bcccafc661b53
“LiciaLicari” <licianbsplicari@gmail.com>
2023-11-08 18:24:47 +0100

    add projects

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,10 @@ Route::get('/', function () {
 /*Admin route*/
 Route::middleware('auth', 'verified')->prefix('admin')->name('admin.')->group(function () {
     Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+
+    Route::resource('projects', ProjectController::class)->parameters([
+        'projects' => 'project:slug'
+    ]);
 });
 
 
