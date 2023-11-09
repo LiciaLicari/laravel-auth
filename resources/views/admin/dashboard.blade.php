7df6717e7185ed0cc41229571b01a466f47987a0
“LiciaLicari” <licianbsplicari@gmail.com>
2023-11-09 17:49:29 +0100

    edit dashboard.blade + index.blade

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -3,16 +3,16 @@
 @section('content')
     <div class="container">
         <div class="d-flex justify-content-between py-4 align-items-center">
-
             <h2 class="fs-4 text-secondary my-4">
-                {{ __('Dashboard') }}
+                Welcome {{ Auth::user()->name }} !
             </h2>
-            <a class="btn btn-primary" href="{{ route('admin.projects.index') }}">Table Project</a>
+
+            <a class="btn btn-primary" href="{{ route('admin.projects.index') }}"> Table Project </a>
         </div>
-        <div class="row justify-content-center">
+        {{-- <div class="row justify-content-center">
             <div class="col">
                 <div class="card">
-                    <div class="card-header">{{ __('User Dashboard') }}</div>
+                    <div class="card-header">Welcome {{ Auth::user()->name }}!</div>
 
                     <div class="card-body">
                         @if (session('status'))
@@ -20,12 +20,37 @@
                                 {{ session('status') }}
                             </div>
                         @endif
-
-                        Welcome {{ Auth::user()->name }}
+                    </div>
+                </div>
+            </div>
+        </div> --}}
+        <div class="py-4 row row-cols-1 row-cols-sm-2 row-cols-md-3">
+            <div class="col">
+                <div class="card h-100">
+                    <div class="card-body">
+                        <h4 class="card-title">Projects</h4>
+                        <p class="card-text">The projects I made</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card h-100">
+                    <div class="card-body">
+                        <h4 class="card-title">About me</h4>
+                        <p class="card-text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Asperiores,
+                            exercitationem sint laborum magni nam fugit laboriosam animi repellat corrupti laudantium sit
+                            pariatur debitis ipsum deserunt placeat alias sunt expedita sapiente!</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card h-100">
+                    <div class="card-body">
+                        <h4 class="card-title">Group projects</h4>
+                        <p class="card-text">Class 104 projects</p>
                     </div>
                 </div>
             </div>
         </div>
-
     </div>
 @endsection
--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -2,55 +2,96 @@
 
 @section('content')
     <div class="container py-4">
+
+
         @if (session('message'))
-            <div class="alert alert-success" role="alert">
-                <strong>Success!</strong>{{ session('message') }}
+            <div class="alert alert-success alert-dismissible fade show" role="alert">
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                <strong>Success!</strong> {{ session('message') }}
             </div>
         @endif
-    </div>
 
-    <h1>Projects Table</h1>
+        <h1>Projects table</h1>
 
-    <div class="d-flex justify-content-between my-4">
-        <a class="btn btn-primary" href="{{ route('admin.projects.create') }}">Add New project</a>
-    </div>
+        <div class="d-flex justify-content-between my-4">
+            <a class="btn btn-primary" href="{{ route('admin.projects.create') }}">Add New project</a>
+        </div>
 
-    <div class="table-responsive">
-        <table class="table table-primary">
-            <thead>
-                <tr>
-                    <th scope="col">ID</th>
-                    <th scope="col">Title</th>
-                    <th scope="col">Image</th>
-                    <th scope="col">Description</th>
-                    <th>Options</th>
-                </tr>
-            </thead>
-            <tbody class="table-group-divider">
-
-
-                @forelse ($projects as $project)
-                    <tr class="">
-                        <td scope="row">{{ $project->id }}</td>
-                        <td>{{ $project->title }}</td>
-                        <td>
-                            <img width="100" class="img-fluid" src="{{ $project->cover_image }}" alt="">
-                        </td>
-                        <td>{{ $project->description }}</td>
-
-                        <td class="d-flex gap-2">
-                            {{-- <a href="{{ route('admin.projects.show', $project->slug) }}"
-                                class="btn btn-outline-primary"></a> --}}
-                            <!-- TO DO edit , delete -->
-                        </td>
+        <div class="table-responsive">
+            <table class="table table-primary table-hover table-striped table-bordered">
+                <thead>
+                    <tr>
+                        <th scope="col">ID</th>
+                        <th scope="col">Title</th>
+                        <th scope="col">Image</th>
+                        <th scope="col">Description</th>
+                        <th scope="col">Options</th>
                     </tr>
+                </thead>
+                <tbody class="table-group-divider">
+                    @forelse ($projects as $project)
+                        <tr>
+                            <td scope="row"> {{ $project->id }} </td>
+                            <td> {{ $project->title }} </td>
 
-                @empty
-                    <p>No projects yet!</p>
-                @endforelse
+                            <td>
 
-            </tbody>
-        </table>
-        {{ $projects->links('pagination::bootstrap-5') }}
+                                <img width="100" class="img-fluid" src="{{ $project->cover_image }}" alt="">
+
+                            </td>
+
+                            <td> {{ $project->description }} </td>
+
+                            <td scope="row" class="py-5 d-flex gap-2 h-100">
+                                <a href=" {{ route('admin.projects.show', $project->slug) }} "
+                                    class="btn btn-outline-primary">View</a>
+                                <a href=" {{ route('admin.projects.edit', $project->slug) }} "
+                                    class="btn btn-outline-success">Edit</a>
+
+                                <!-- Modal trigger button -->
+                                <button type="button" class="btn btn-outline-danger" data-bs-toggle="modal"
+                                    data-bs-target="#modalId-{{ $project->id }}">
+                                    Delete
+                                </button>
+
+                                <!-- Modal Body -->
+                                <!-- if you want to close by clicking outside the modal, delete the last endpoint:data-bs-backdrop and data-bs-keyboard -->
+                                <div class="modal fade" id="modalId-{{ $project->id }}" tabindex="-1"
+                                    data-bs-backdrop="static" data-bs-keyboard="false" role="dialog"
+                                    aria-labelledby="modalTitleId-{{ $project->id }}" aria-hidden="true">
+                                    <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered modal-sm"
+                                        role="document">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h5 class="modal-title" id="modalTitleId-{{ $project->id }}">Warning!</h5>
+                                                <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                                    aria-label="Close"></button>
+                                            </div>
+                                            <div class="modal-body">
+                                                Are you sure? This is an irreversible action
+                                            </div>
+                                            <div class="modal-footer">
+                                                <button type="button" class="btn btn-secondary"
+                                                    data-bs-dismiss="modal">Close</button>
+                                                <form action="{{ route('admin.projects.destroy', $project->slug) }}"
+                                                    method="POST">
+                                                    @csrf
+                                                    @method('DELETE')
+                                                    <button type="submit" class="btn btn-danger">Confirm</button>
+                                                </form>
+                                            </div>
+                                        </div>
+                                    </div>
+                            </td>
+                        </tr>
+
+                    @empty
+                        No projects yet
+                    @endforelse
+                </tbody>
+            </table>
+
+            {{ $projects->links('pagination::bootstrap-5') }}
+        </div>
     </div>
 @endsection
