aed168d6167a78f4018f233e5f18dc5117f07571
“LiciaLicari” <licianbsplicari@gmail.com>
2023-11-09 17:16:45 +0100

    add show.blade

---
+++ resources/views/admin/projects/show.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.admin')
+
+@section('content')
+    <div>
+        Title:
+        {{ $project->title }}
+    </div>
+
+    <div>
+        Id:
+        {{ $project->id }}
+    </div>
+@endsection
+
+
+{{-- make it better --}}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-// use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\ProjectController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
@@ -22,7 +22,7 @@ Route::get('/', function () {
 
 
 Route::middleware('auth', 'verified')->prefix('admin')->name('admin.')->group(function () {
-    // Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+    Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
     Route::resource('/projects', ProjectController::class)->parameters([
         'projects' => 'project:slug'
     ]);
